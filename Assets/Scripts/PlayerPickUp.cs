d5cd52f78844c228d62c3ccd3dabf81b5b628614
Tomáš Vik <GepardCZE@post.cz>
2023-04-17 21:29:53 +0200

    ?

--- Assets/Scripts/PlayerPickUp.cs
+++ Assets/Scripts/PlayerPickUp.cs
@@ -9,6 +9,7 @@ public class PlayerPickUp : MonoBehaviour
     public GameObject pickupPosition;
     public int pickupCount = 0;
     public int pickupRange = 1;
+    public GameObject currentPickupObject;
     
     public int fluidLayer;
     // Start is called before the first frame update
@@ -22,6 +23,8 @@ public class PlayerPickUp : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
             PickUp();
+        if (Input.GetKeyDown(KeyCode.Mouse1) && currentPickup != PickupType.None)
+            DeployPickup();
     }
 
     private void PickUp()
@@ -30,14 +33,26 @@ public class PlayerPickUp : MonoBehaviour
         foreach (Collider2D collider in colliders)
         {
             Debug.Log(collider.gameObject.layer + " " + fluidLayer);
-            if (collider.gameObject.layer == fluidLayer)
+            if (collider.gameObject.layer == fluidLayer && currentPickup == PickupType.None && currentPickupObject == null)
             {
-                currentPickup = collider.gameObject.transform.tag == "Water" ? PickupType.Water : PickupType.None;
-                
+                currentPickupObject = collider.gameObject;
+                currentPickup = PickupType.Water;
                 pickupCount++;
+                
                 Destroy(collider.gameObject);
             }
         }
     }
-    
+    private void DeployPickup()
+    {
+        if(pickupCount > 0)
+            pickupCount--;
+        else
+            return;
+        
+        GameObject GO = Instantiate(currentPickupObject, pickupPosition.transform.position, Quaternion.identity);
+        GO.GetComponent<Rigidbody2D>().AddForce(new Vector2(pickupPosition.transform.localPosition.x, pickupPosition.transform.localPosition.y));
+        
+        
+    }
 }
